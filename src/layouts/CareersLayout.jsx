5e7c3cb368fed3ce523140c1039dd843d2edb54f
Abdul <akoredebakare4u@gmail.com>
2023-05-25 11:22:29 -0300

    update content

--- src/layouts/CareersLayout.jsx
+++ src/layouts/CareersLayout.jsx
@@ -5,9 +5,15 @@ export default function CareersLayout() {
     <div className="careers-layout">
       <h2 className="text-2xl font-bold">Careers</h2>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias eum nemo
-        non vitae iusto in dicta? Velit similique repellendus, unde qui
-        assumenda, eaque cumque commodi eligendi, impedit illo suscipit facilis.
+        Welcome to our careers page! We're thrilled that you're considering
+        joining our dynamic and innovative team. we believe that our greatest
+        asset is our people, and we strive to create an environment where
+        individuals can thrive and grow both personally and professionally. Our
+        commitment to excellence, collaboration, and continuous learning is what
+        sets us apart. If you're passionate about making a meaningful impact,
+        embracing new challenges, and being part of a supportive and diverse
+        community, then you've come to the right place. Explore the exciting
+        opportunities below and embark on an incredible journey with us.
       </p>
       <Outlet />
     </div>
--- src/layouts/HelpLayout.jsx
+++ src/layouts/HelpLayout.jsx
@@ -5,16 +5,22 @@ export default function HelpLayout() {
     <div className="help-layout">
       <h2 className="text-2xl font-bold">Website Help</h2>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nesciunt,
-        minima nam voluptas natus dolores itaque reprehenderit officiis commodi
-        at velit, recusandae in quibusdam quis facere. Eligendi voluptate
-        assumenda explicabo.
+        Welcome to our help page! We're here to assist you and ensure that your
+        experience with our outlet is smooth and enjoyable. Whether you have
+        questions, encounter technical issues, or need guidance on how to make
+        the most of our services, you've come to the right place. Our team of
+        dedicated support agents is ready to provide prompt and comprehensive
+        assistance. Browse through our helpful resources, frequently asked
+        questions, and troubleshooting guides to find the answers you need. If
+        you can't find a solution, don't hesitate to reach out to us directly.
+        We're committed to resolving your concerns and making your time with us
+        a positive one.
       </p>
       <nav>
         <NavLink to="faq">View the FAQ</NavLink>
         <NavLink to="contact">Contact Us</NavLink>
       </nav>
-      <Outlet/>
+      <Outlet />
     </div>
   );
 }
